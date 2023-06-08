7370637c3cebf33e3a33e1c95c47c242656f1c7c
ModernMon <lakshmiaravind.atom@gmail.com>
2023-06-08 12:06:04 +0530

    sent a sample email from newsletter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from  send_mail import send_email
 
 api_key = "0c4f74204b1b41f8b94ae9aee304087e"
 
@@ -10,6 +11,15 @@ url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=0c4f74204b1b41f8
 response = requests.get(url)
 content = response.json()
 
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 3*"\n"
+
+
+body = body.encode("utf-8")
+send_email(message=body)
+
+
+
